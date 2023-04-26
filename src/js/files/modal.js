83665206ebb5a58080a9befd572a6f0ebba9ffcc
Viktor <70236188+monsherok@users.noreply.github.com>
2023-04-26 17:46:15 +0500

    Новые файлы

---
+++ src/js/files/modal.js
@@ -0,0 +1,47 @@
+export default class Modal {
+    constructor() {
+        this.datas = {
+            modalWindow: 'data-modal',
+            modalBtn: 'data-modal-btn',
+            modalClose: 'data-close'
+        }
+        this.classes = {
+            open: 'modal__open',
+            content: 'modal-content',
+            lock: 'lock'
+        }
+        this.open = false;
+        this.activeModalClass = ''
+        this.init()
+    }
+
+    init() {
+        document.addEventListener('click', e => {
+            if (e.target.classList.contains('open-modal')) {
+                
+                this.modalOpen(e)
+            } else if (e.target.closest(`.${this.classes.open}`)) {
+                if (!e.target.closest(`.${this.classes.content}`) || e.target.hasAttribute(`${this.datas.modalClose}`)) {
+                    e.preventDefault()
+                    this.toggleModal(document.getElementById(`${this.activeModalClass}`))
+                }
+            }
+        })
+    }
+
+    modalOpen(e) {
+        e.preventDefault()
+        const modal = document.querySelector(`#${e.target.dataset.modalBtn}`)
+        this.activeModalClass = e.target.dataset.modalBtn
+        console.log(this.activeModalClass);
+        if (modal) {
+            this.toggleModal(modal)
+
+        }
+    }
+
+    toggleModal(modal) {
+        modal.classList.toggle('modal__open')
+        document.documentElement.classList.toggle('lock')
+    }
+}
