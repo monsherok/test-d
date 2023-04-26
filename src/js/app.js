4aa92e82fef56ee544c04bb7d89404658461e20e
Viktor <70236188+monsherok@users.noreply.github.com>
2023-04-26 17:40:19 +0500

    Переписал код для модальных окон

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 
 // Подключение основного файла стилей
 import "../scss/style.scss";
-
+import Modal from "./files/modal.js"
 import "./files/script.js";
+
+// Вызываем класс с модальными окнами
+new Modal()
