03904afa5970e07cad795b9efe0e2faf366d6bf4
Viktor <70236188+monsherok@users.noreply.github.com>
2023-05-25 00:50:18 +0500

    рефакторинг

--- src/js/files/form.js
+++ src/js/files/form.js
@@ -1,130 +1,123 @@
 import modal from "./modal.js";
 
-var token = '6111024453:AAHnDkXdv7lgRyQD4vzyUiKwgW7MFsuerHQ';
-var chat_id = '@monshero';
+const token = '6111024453:AAHnDkXdv7lgRyQD4vzyUiKwgW7MFsuerHQ';
+const chat_id = '@monshero';
 
-document.body.addEventListener("focusout", function (e) {
-    const targetElement = e.target;
-    if ((targetElement.tagName === 'INPUT' || targetElement.tagName === 'TEXTAREA')) {
-        if (targetElement.hasAttribute('data-required')) {
-            formValidate.checkValidate(targetElement);
-        }
-    }
-});
+document.body.addEventListener("focusout", handleFocusOut);
 
-export let formValidate = {
+export const formValidate = {
     checkValidate(input) {
-        let error = 0
+        let error = 0;
         if (input.dataset.required === 'email') {
             if (this.emailTest(input)) {
-                error++
-                this.addError(input)
+                error++;
+                this.addError(input);
             } else {
-                this.removeError(input)
+                this.removeError(input);
             }
-            return error
+            return error;
         }
 
         if (!input.value) {
-            error++
-            this.addError(input)
+            error++;
+            this.addError(input);
         } else {
-            this.removeError(input)
+            this.removeError(input);
         }
-        return error
+        return error;
     },
     addError(requiredElement) {
-        requiredElement.classList.add('error')
+        requiredElement.classList.add('error');
     },
     removeError(requiredElement) {
-        requiredElement.classList.remove('error')
+        requiredElement.classList.remove('error');
     },
     emailTest(requiredElement) {
         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(requiredElement.value);
     }
-}
+};
 
 export function formSubmit() {
     const forms = document.forms;
     if (forms.length) {
         for (const form of forms) {
-            form.addEventListener('submit', function (e) {
-                e.preventDefault()
-                const form = e.target;
-                validateFormInputs(form)
-            });
-            form.addEventListener('reset', function (e) {
-                const form = e.target;
-                form.reset()
-            });
+            form.addEventListener('submit', handleSubmit);
+            form.addEventListener('reset', handleReset);
         }
     }
+}
 
-    function validateFormInputs(form) {
-        const requiredInputs = form.querySelectorAll('[data-required]');
-        let errors = 0
-        if (requiredInputs.length) {
-            requiredInputs.forEach(requiredInput => {
-                errors += formValidate.checkValidate(requiredInput)
-            })
-        }
-        if (errors === 0) {
-            holdSendButton(form)
-            formFetch(form)
-        }
+function handleFocusOut(e) {
+    const targetElement = e.target;
+    if (targetElement.matches('input[data-required], textarea[data-required]')) {
+        formValidate.checkValidate(targetElement);
     }
+}
 
-    async function formFetch(form) {
-        var form_data = new FormData(form);
-        setTextToElement(form, 'SENDING...')
-        // отправка данных формы в Telegram Bot API
-        fetch('https://api.telegram.org/bot' + token + '/sendMessage', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-        //   body: 'chat_id=' + chat_id + '&text=' + form_data
-        body: 'chat_id=' + chat_id + '&text=' + JSON.stringify(Object.fromEntries(form_data.entries()))
-        })
-        .then(function(response) {
-          if (response.ok) {
-            // уведомление об успешной отправке
-            form.reset()
-            modal.toggleModal(form.closest('[data-modal]'))
-            openPopup('Успешная отправка формы')
-          } else {
-            // уведомление о неуспешной отправке
-            openPopup('Ошибка отправки формы!');
-          }
-        })
-        .catch(function(error) {
-          // уведомление об ошибке
-          openPopup('Ошибка: ' + error.message);
-        })
-        .finally( () => {
-            holdSendButton(form)
-            setTextToElement(form, 'SUBMIT')
-        })
+function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    validateFormInputs(form);
+}
+
+function handleReset(e) {
+    const form = e.target;
+    form.reset();
+}
+
+async function validateFormInputs(form) {
+    const requiredInputs = form.querySelectorAll('[data-required]');
+    let errors = 0;
+    if (requiredInputs.length) {
+        requiredInputs.forEach(requiredInput => {
+            errors += formValidate.checkValidate(requiredInput);
+        });
     }
+    if (errors === 0) {
+        holdSendButton(form);
+        formFetch(form);
+    }
+}
 
-    function holdSendButton(form) {
-        const sendButton = form.querySelector('button[type="submit"]')
-        if (sendButton.hasAttribute('disabled')) {
-            sendButton.disabled = false
+async function formFetch(form) {
+    const form_data = new FormData(form);
+    setTextToElement(form, 'SENDING...');
+    try {
+        const response = await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `chat_id=${chat_id}&text=${JSON.stringify(Object.fromEntries(form_data.entries()))}`
+        });
+        if (response.ok) {
+            form.reset();
+            modal.toggleModal(form.closest('[data-modal]'));
+            openPopup('Успешная отправка формы');
         } else {
-            sendButton.disabled = true
+            openPopup('Ошибка отправки формы!');
         }
+    } catch (error) {
+        openPopup(`Ошибка: ${error.message}`);
+    } finally {
+        holdSendButton(form);
+        setTextToElement(form, 'SUBMIT');
     }
+}
 
-    function setTextToElement(form, text) {
-        const sendButton = form.querySelector('button[type="submit"]')
-        sendButton.innerHTML = text
-    }
+function holdSendButton(form) {
+    const sendButton = form.querySelector('button[type="submit"]');
+    sendButton.disabled = !sendButton.disabled;
+}
 
-    function openPopup(message) {
-        let infoModal = document.getElementById('modal-2')
-        modal.activeModalClass = infoModal.id
-        infoModal.querySelector('.modal__title').innerHTML = message
-        modal.toggleModal(infoModal)
-    }
+function setTextToElement(form, text) {
+    const sendButton = form.querySelector('button[type="submit"]');
+    sendButton.innerHTML = text;
+}
+
+function openPopup(message) {
+    const infoModal = document.getElementById('modal-2');
+    modal.activeModalClass = infoModal.id;
+    infoModal.querySelector('.modal__title').innerHTML = message;
+    modal.toggleModal(infoModal);
 }
